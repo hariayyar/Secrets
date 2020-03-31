fde268f772d903e96dfbb8ec5425e82d70d22b2d
Hari Ayyar <hariayyar@Haris-MacBook-Air.local>
2020-03-30 22:10:34 -0400

    Completed Secrets App

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const userSchema = new mongoose.Schema ({
   password: String,
   googleId: String,
   facebookId: String,
+  secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -111,8 +112,6 @@ app.get('/auth/facebook/secrets',
     res.redirect('/secrets');
   });
 
-
-
 app.get("/login", function (req, res) {
   res.render("login");
 });
@@ -122,8 +121,21 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+
+  User.find({"secret": {$ne: null}}, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {foundUsers: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function (req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
@@ -164,6 +176,23 @@ app.post("/login", function (req, res) {
       passport.authenticate("local")(req, res, function() {
         res.redirect("/secrets");
       });
+    }
+  });
+
+});
+
+app.post("/submit", function (req, res) {
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = req.body.secret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        });
+      }
     }
   });
 
